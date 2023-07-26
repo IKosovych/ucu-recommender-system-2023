64aef7461691d187fee47fea7159309f7c3b2137
s-shostak-UCU <123777921+s-shostak-UCU@users.noreply.github.com>
2023-07-26 14:15:16 +0300

    Update baseline_model

--- models/baseline_model.py
+++ models/baseline_model.py
@@ -7,12 +7,27 @@ from data_uploader.uploader import DataUploader
 
 class BaselineModel:
     """
-    A baseline recommendation model that recommends movies 
+    A baseline recommendation model that recommends movies
     based on their popularity and average ratings
     """
     def __init__(self):
         data_uploader = DataUploader()
-        _, self.train_set, self.test_set = data_uploader.get_user_item_data_surprise()
+        self.df_movies, _, self.train_set, self.test_set = data_uploader.get_user_item_data_for_cbf()
+        self.train_set = self.trainset_to_dataframe(self.train_set)
+
+
+    def trainset_to_dataframe(self,trainset):
+        user_ids = []
+        item_ids = []
+        ratings = []
+        for uid, iid, rating in trainset.all_ratings():
+            user_ids.append(trainset.to_raw_uid(uid))
+            item_ids.append(trainset.to_raw_iid(iid))
+            ratings.append(rating)
+
+        df = pd.DataFrame({'userId': user_ids, 'movieId': item_ids, 'rating': ratings})
+        return df
+
 
     def fit(self):
         self.train_set['count'] = 1
@@ -26,9 +41,12 @@ class BaselineModel:
 
     def predict_on_testset(self, user_id):
         filtered_films = self.df_ranked_films.merge(self.train_set[self.train_set['userId'] == user_id]['movieId'], 
-                                                     on='movieId', 
-                                                     how='left', 
-                                                     indicator=True)
+                                                    on='movieId', 
+                                                    how='left', 
+                                                    indicator=True)
 
         filtered_films = filtered_films[filtered_films['_merge'] == 'left_only']
-        return filtered_films['movieId'].head(10)
+        # drop duplicates and get top 10 recommendations
+        top_10_recs = filtered_films.drop_duplicates(subset=['movieId']).head(10)
+        top_10_recs = top_10_recs.merge(self.df_movies, on='movieId')
+        return top_10_recs[['movieId', 'title', 'rank']]
