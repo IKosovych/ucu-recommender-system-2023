fb2d3d20f298bd35ca0f9bba60c1ea0baf34b761
s-shostak-UCU <123777921+s-shostak-UCU@users.noreply.github.com>
2023-08-23 12:53:26 +0300

    Updated ab_distributor.py

--- ab_distributor.py
+++ ab_distributor.py
@@ -9,11 +9,7 @@ from surprise import Dataset, Reader
 
 class ABtestDistributor:
     def __init__(self, data_uploader):
-        # Use the DataUploader instance
-        self.df_movies = data_uploader.df_movies
-        self.df_ratings = data_uploader.df_ratings
-        self.data = data_uploader.data  # Assuming data_uploader has a 'data' attribute
-
+        self.data_uploader = data_uploader
         self.user_list = pd.DataFrame()
         self.model_a = ''
         self.model_b = ''
@@ -22,7 +18,7 @@ class ABtestDistributor:
         # create base list of users
         self.model_a = model_a
         self.model_b = model_b
-        self.user_list['userId'] = self.df_ratings['userId'].unique()
+        self.user_list['userId'] = self.data_uploader.df_ratings['userId'].unique()
         self.user_list['model'] = model_a
 
         feature_list = self.calculate_user_features()
@@ -31,8 +27,7 @@ class ABtestDistributor:
         for n in range(n_clusters):
             ids = self.user_list[self.user_list['user_type'] == n]['userId']
             num_elements = int(split * len(ids))
-            
-             # Randomly select 30% of elements from the array
+
             selected_elements = np.random.choice(ids, size=num_elements, replace=False)
 
             self.user_list.loc[self.user_list['userId'].isin(selected_elements), 'model'] = model_b
@@ -40,33 +35,28 @@ class ABtestDistributor:
     def user_exists(self, user_id):
         return not self.user_list[self.user_list['userId'] == user_id].empty
 
-    def get_model_name(self, user_id):
-        return self.user_list[self.user_list['userId'] == user_id].model.values[0]
-
-
     def cluster_users(self, features, n_clusters=2):
         X = self.user_list[features].to_numpy(dtype='int')
 
         kmeans = KMeans(n_clusters=n_clusters, random_state=0).fit(X)
         self.user_list['user_type'] = pd.Series(kmeans.labels_)
-
+        
+    def get_model_name(self, user_id):
+        return self.user_list[self.user_list['userId'] == user_id].model.values[0]
+        
     def calculate_user_features(self):
         self.user_list = self.user_list.set_index('userId')
-        self.user_list['total_rating_count'] = self.df_ratings.groupby('userId')['rating'].count()
-        self.user_list['total_rating_sum'] = self.df_ratings.groupby('userId')['rating'].sum()
+        self.user_list['total_rating_count'] = self.data_uploader.df_ratings.groupby('userId')['rating'].count()
+        self.user_list['total_rating_sum'] = self.data_uploader.df_ratings.groupby('userId')['rating'].sum()
         self.user_list = self.user_list.reset_index()
 
         return ['total_rating_count', 'total_rating_sum']
 
     def log_interaction(self, user_id, movie_id, action):
-        # Logs for evaluation, action could be 'watch', 'click', 'rate', etc.
-        # For simplicity, we just append logs to a CSV.
         with open('interaction_log.csv', 'a') as file:
             file.write(f"{user_id},{movie_id},{action}\n")
 
     def evaluate_models(self):
-        # Here, we read from the interaction_log and perform your evaluation
-        # For demonstration purposes, let's just count interactions.
         try:
             interactions = pd.read_csv('interaction_log.csv', names=['userId', 'movieId', 'action'])
             model_a_interactions = interactions[interactions['userId'].isin(self.user_list[self.user_list['model'] == self.model_a]['userId'])]
