56ff370d2bd28ff0014be33e5f37fda788a30967
s-shostak-UCU <123777921+s-shostak-UCU@users.noreply.github.com>
2023-08-23 21:24:03 +0300

    Added evaluation to content_based_filtering.py

--- models/content_based_filtering.py
+++ models/content_based_filtering.py
@@ -59,3 +59,29 @@ class ContentBasedFiltering:
 
         # Return the top 10 most similar movies
         return self.movies['title'].iloc[movie_indices]
+
+    def evaluate_ndcg(self):
+        users = self.test_set['userId'].unique()
+        average_ndcg = []
+
+        for user in users:
+            actual = self.test_set[self.test_set['userId'] == user]['movieId'].tolist()
+            predicted = self.predict_on_testset(user)
+
+            # Convert movie titles back to ids for evaluation
+            predicted_ids = self.df_movies[self.df_movies['title'].isin(predicted)]['movieId'].tolist()
+
+            lb = LabelBinarizer()
+            lb.fit(actual)
+
+            if len(predicted_ids) <= len(actual):
+                actual = actual[:len(predicted_ids)]
+            else:
+                predicted_ids = predicted_ids[:len(actual)]
+
+            binary_actual = lb.transform(actual)
+            binary_predicted = lb.transform(predicted_ids)
+
+            average_ndcg.append(ndcg_score(binary_actual, binary_predicted))
+
+        return np.mean(average_ndcg)
